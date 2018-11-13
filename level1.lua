23d2b7bed23b2eb40fc41cff332417119de00474
Travis Halleck <hallectj@gmail.com>
2018-11-13 12:28:12 -0600

    added launch source and destination points

--- level1.lua
+++ level1.lua
@@ -11,6 +11,67 @@ local scene = composer.newScene()
 local foreground
 local launchPadLeft
 local launchPadRight
+local padTopLeft 
+local padTopRight
+
+local myCircle
+local circle1
+local circle2
+
+local squareHt = 10
+local squareWd = 10
+
+
+
+--text:setTextColor( unpack(myColor) )
+
+local squaresBottomLeftArray;
+local squaresBottomRightArray;
+local squaresTopLeftArray;
+local squaresTopRightArray;
+ 
+function createSquares(orientation, length) 
+	local blue = { 0.25, 0.25, 0.75 }
+    local gray = { 0.50, 0.50, 0.50 }
+	local squaresArry, offset, squareY, squareX, identifier = {}, 20, 0, 0, "";
+	if(orientation == "bottom left") then 
+		squareY = 280
+		squareX = 0
+		identifier = "bottom left"
+	elseif(orientation == "bottom right") then 
+		squareY = 280
+		squareX = 340
+		identifier = "bottom right"
+	elseif(orientation == "top left") then 
+		--padTopLeft = display.newRect(130, 150, 245, 20)
+		squareY = 150
+		squareX = 0
+		identifier = "top left"
+	elseif(orientation == "top right") then 
+		squareY = 150 
+		squareX = 340 
+		identifier = "top right"
+	end
+
+	for i = 1, length do 
+		squareX = squareX + offset
+		local square = display.newRect(squareX, squareY, squareHt, squareWd);
+		if(orientation == "top left" or orientation == "bottom right") then 
+		  square:setFillColor(unpack(blue))
+		elseif(orientation == "top right" or orientation == "bottom left") then 
+		  square:setFillColor(unpack(gray))
+		end
+		square.identifier = identifier
+		table.insert(squaresArry, square)
+	end
+	return squaresArry
+end
+
+
+squaresBottomLeftArray = createSquares("bottom left", 10)
+squaresBottomRightArray = createSquares("bottom right", 10)
+squaresTopLeftArray = createSquares("top left", 10)
+squaresTopRightArray = createSquares("top right", 10)
 
 -- next scene
 local function levelEventListener( event )
@@ -31,14 +92,18 @@ function scene:create( event )
 	foreground.anchorX = 0
 	foreground.anchorY = 0
 
-	launchPadLeft = display.newRect(130, 280, 245, 20)
-	launchPadRight = display.newRect(442, 280, 245, 20)
-
+	launchPadLeftBottom = display.newRect(130, 280, 245, 20)
+	launchPadRightBottom = display.newRect(442, 280, 245, 20)
+	padTopLeft = display.newRect(130, 150, 245, 20)
+	padTopRight = display.newRect(442, 150, 245, 20)
 
 	sceneGroup:insert(foreground)
-	sceneGroup:insert(launchPadLeft)
-	sceneGroup:insert(launchPadRight)
-	-- Initialize the scene here.
+	sceneGroup:insert(launchPadLeftBottom)
+	sceneGroup:insert(launchPadRightBottom)
+	sceneGroup:insert(padTopLeft)
+	sceneGroup:insert(padTopRight)
+
+	--  Initialize the scene here.
 	-- Example: add display objects to "sceneGroup", add touch listeners, etc.
 end
 
@@ -82,6 +147,8 @@ scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
+
+--Runtime:addEventListener( "enterFrame", tmp )
 ---------------------------------------------------------------------------------
 
 return scene
