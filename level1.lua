de3ed3e43c33d146868c7031d86c0a25c657f380
Travis Halleck <hallectj@gmail.com>
2018-11-17 21:21:35 -0600

    added scoring, and poof animation sprites

--- level1.lua
+++ level1.lua
@@ -9,13 +9,27 @@ local Trajectory = require( "dmc_trajectory.DMC-trajectory-basic.dmc_library.dmc
 -- local forward references should go here
 ---------------------------------------------------------------------------------
 local LENGTH = 10
+local score = 0
+local scoreText
+
+local sheet
+local smoke_opt
+local smoke_seq
+local animSmoke
+
+local topHalfArr, bottomHalfArr = {}, {}
 
 local foreground
 local clayPigeon1 = nil
+local clayPigeon2 = nil 
 local launchComplete = true 
 
-local clayPigeonIteration = 8000
-local speed = 5000
+local clay1ID = 0
+local clay2ID = 0
+local dashboard
+
+local clayPigeonIteration = 7000
+local speed = 4000
 local amountOfClayPigeons = 10
 local scaleFactor = (speed / ((amountOfClayPigeons/2) * 100)) * 100
 
@@ -34,21 +48,25 @@ local squaresTopLeftArray;
 local squaresTopRightArray;
 
 
+local soundTable = {
+	explosionSound = audio.loadSound("explosion.wav"),
+}
+
 function createSquares(orientation) 
 	local blue = { 0.25, 0.25, 0.75 }
     local gray = { 0.50, 0.50, 0.50 }
 	local squaresArry, offset, squareY, squareX, identifier = {}, 20, 0, 0, "";
 	if(orientation == "bottom left") then 
-		squareY = 280
+		squareY = 220
 		squareX = 0
 	elseif(orientation == "bottom right") then 
-		squareY = 280
+		squareY = 220
 		squareX = 340
 	elseif(orientation == "top left") then
-		squareY = 150
+		squareY = 140
 		squareX = 0
 	elseif(orientation == "top right") then 
-		squareY = 150 
+		squareY = 140
 		squareX = 340 
 	end
 
@@ -62,6 +80,7 @@ function createSquares(orientation)
 		end
 		square.identifier = orientation
 		square.xPos = square.x 
+		square.alpha = 0.01;
 		table.insert(squaresArry, square)
 	end
 	return squaresArry
@@ -76,50 +95,130 @@ function selectRandomSquare(arr)
 	return randSquare;
 end 
 
+function populateDashoardCircles()
+	local upperHalfY = 280
+	local lowerHalfY = 300
+	local identifier = ""
+	local offset, circleY, circleX = 15, upperHalfY, 0;
+	for i = 1, 10 do 
+		circleX = circleX + offset
+		local circle1 = display.newCircle(circleX, upperHalfY, 5)
+		local circle2 = display.newCircle(circleX, lowerHalfY, 5)
+		circle1:setStrokeColor(0,0,0)
+		circle1.strokeWidth = 3
+		circle1.identifier = "top " .. i 
+		table.insert(topHalfArr, circle1)
+
+		circle2:setStrokeColor(0,0,0)
+		circle2.strokeWidth = 3
+		circle2.identifier = "bottom " .. i
+		table.insert(bottomHalfArr, circle2)
+	end
+end
+
+populateDashoardCircles()
 
 
+function cleanUpSmoke()
+	animSmoke.isVisible = false 
+end
 
-function onTouchEvent(event) 
+function onTouchEventClay1(event) 
   if(event.phase == "began") then 
-    print("you touched me")
+	clayPigeon1.isVisible = false;
+	animSmoke = display.newSprite (sheet, smoke_seq);
+	animSmoke.isVisible = true 
+	animSmoke.x = clayPigeon1.x 
+	animSmoke.y = clayPigeon1.y
+	animSmoke:setSequence("explode")
+	animSmoke:play()
+	timer.performWithDelay(400, cleanUpSmoke, 1)
+	audio.play(soundTable["explosionSound"])
+	score = score + 10 
+	scoreText.text = "score " .. score 
+	for i = 1, table.getn(topHalfArr) do 
+		if(topHalfArr[i].identifier == clayPigeon1.identifier) then 
+			topHalfArr[i]:setFillColor(1, 0, 0)
+		end
+	end
   end
 end
+
+function onTouchEventClay2(event) 
+	if(event.phase == "began") then 
+	  clayPigeon2.isVisible = false;
+	  animSmoke = display.newSprite (sheet, smoke_seq);
+	  animSmoke.isVisible = true 
+	  animSmoke.x = clayPigeon2.x 
+	  animSmoke.y = clayPigeon2.y
+	  animSmoke:setSequence("explode")
+	  animSmoke:play()
+	  timer.performWithDelay(400, cleanUpSmoke, 1)
+	  audio.play(soundTable["explosionSound"])
+	  score = score + 10 
+	  scoreText.text = "score " .. score 
+	  for i = 1, table.getn(topHalfArr) do 
+		if(bottomHalfArr[i].identifier == clayPigeon2.identifier) then 
+			bottomHalfArr[i]:setFillColor(1, 0, 0)
+		end
+	end
+	end
+  end
 	
 local function reduceScale( reduceScaleBy )
   local scaleStart = 1.0
   return function()
     scaleStart = scaleStart - reduceScaleBy
     clayPigeon1.xScale = scaleStart
-    clayPigeon1.yScale = scaleStart
+	clayPigeon1.yScale = scaleStart
+	clayPigeon2.xScale = scaleStart
+	clayPigeon2.yScale = scaleStart
   end
 end
 	
-local function setupTransition()	 
+local function setupTransition()	
 	clayPigeon1 = display.newCircle( 0, 0, 25 )
 	clayPigeon1.x, clayPigeon1.y = 1000, -1100
 	clayPigeon1.strokeWidth = 3
     clayPigeon1:setStrokeColor( 0, 0, 0 )
 	clayPigeon1.name = "clay pigeon1"
-	clayPigeon1:addEventListener("touch", onTouchEvent)
+	clayPigeon1:addEventListener("touch", onTouchEventClay1)
 	clayPigeon1.isVisible = false
+
+	clayPigeon2 = display.newCircle( 0, 0, 25 )
+	clayPigeon2.x, clayPigeon1.y = 1000, -1100
+	clayPigeon2.strokeWidth = 3
+    clayPigeon2:setStrokeColor( 0, 0, 0 )
+	clayPigeon2.name = "clay pigeon2"
+	clayPigeon2:addEventListener("touch", onTouchEventClay2)
+	clayPigeon2.isVisible = false
+
 end
 
 
 local function doTransition()
 
-	local heightArr = {60, 80, 100, 120, 140, 160, 165}
+	clay1ID = clay1ID + 1
+	clay2ID = clay2ID + 1
+	local heightArr = {60, 80, 100, 120, 140, 150}
 	local randHeight = 0 
 	local len = table.getn(heightArr)
 	for i = 1, len do 
 		randHeight = heightArr[math.random(1, len)]
+		randHeight2 = heightArr[math.random(1, len)]
 	end 
 	
 	clayPigeon1.isVisible = true
+	clayPigeon2.isVisible = true
+	
+	clayPigeon1.identifier = "top " .. clay1ID
+	clayPigeon2.identifier = "bottom " .. clay2ID
+
 	local onCompleteCallback = function()
 	    launchComplete = true
 		clayPigeon1:removeSelf()
+		clayPigeon2:removeSelf()
 		setupTransition() 
-		
 	end
 
 	if(launchComplete) then 
@@ -127,14 +226,25 @@ local function doTransition()
 		projectileTimer = timer.performWithDelay(scaleFactor, reduceScale(0.20), math.ceil(amountOfClayPigeons/2))
 	end 
 
-	local params = {
+	local params1 = {
 		time = speed,
 		pBegin= {selectRandomSquare(squaresBottomLeftArray).x, selectRandomSquare(squaresBottomLeftArray).y},
 		pEnd= {selectRandomSquare(squaresTopRightArray).x, selectRandomSquare(squaresTopRightArray).y},
 		height= randHeight,
 		onComplete=onCompleteCallback
 	}
-	Trajectory.move( clayPigeon1, params )
+
+	local params2 = {
+		time = speed,
+		pBegin= {selectRandomSquare(squaresBottomRightArray).x, selectRandomSquare(squaresBottomRightArray).y},
+		pEnd= {selectRandomSquare(squaresTopLeftArray).x, selectRandomSquare(squaresTopLeftArray).y},
+		height= randHeight2,
+		onComplete=onCompleteCallback		
+	}
+
+
+	Trajectory.move( clayPigeon1, params1 )
+	Trajectory.move( clayPigeon2, params2 )
 	launchComplete = false
 end
 
@@ -155,17 +265,50 @@ end
 -- "scene:create()"
 function scene:create( event )
 	local sceneGroup = self.view
+
+	smoke_opt = {
+		frames = {
+			{x = 0, y = 0, width = 15, height = 15},
+			{x = 18, y = 0, width = 15, height = 15},
+			{x = 35, y = 0, width = 15, height = 15},
+			{x = 50, y = 0, width = 15, height = 15},
+			{x = 0, y = 16, width = 15, height = 15},
+			{x = 18, y = 16, width = 15, height = 15},
+			{x = 35, y = 16, width = 15, height = 15},
+			{x = 50, y = 16, width = 15, height = 15}
+		}
+	}
+
+	smoke_seq = {
+	    {name = "explode", frames = { 1, 2, 3, 4, 5, 6, 7, 8}, time = 250, loopCount = 1}
+	}
+
+	sheet = graphics.newImageSheet("smoke.png", smoke_opt);
+
+
 	foreground = display.newImage("foreground.png")
 	foreground.width = 640
 	foreground.height = 320
+	foreground.y = -55
 	foreground.anchorX = 0
 	foreground.anchorY = 0
-	
 
 	launchPadLeftBottom = display.newRect(130, 280, 245, 20)
 	launchPadRightBottom = display.newRect(442, 280, 245, 20)
 	padTopLeft = display.newRect(130, 150, 245, 20)
 	padTopRight = display.newRect(442, 150, 245, 20)
+
+	dashboard = display.newRect(285, 290, display.actualContentWidth-5, 50)
+	dashboard:setStrokeColor(0,0,0)
+	dashboard.strokeWidth = 3
+
+	scoreText = display.newText("score: " .. score, 410, 290, native.systemFont, 24)
+	scoreText:setFillColor(1, 0, 0)
+
+	launchPadLeftBottom.alpha = 0.01
+	launchPadRightBottom.alpha = 0.01
+	padTopLeft.alpha = 0.01 
+	padTopRight.alpha = 0.01
 
 	squaresBottomLeftArray = createSquares("bottom left", 10)
     squaresBottomRightArray = createSquares("bottom right", 10)
@@ -177,6 +320,7 @@ function scene:create( event )
 	sceneGroup:insert(launchPadRightBottom)
 	sceneGroup:insert(padTopLeft)
 	sceneGroup:insert(padTopRight)
+	sceneGroup:insert(dashboard)
 
 	launchFunc()
     setupTransition()
